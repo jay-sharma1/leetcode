64886743612c472a3cac471e24be0cb40405a461
jay-sharma1 <64980964+jay-sharma1@users.noreply.github.com>
2023-05-31 09:12:01 -0400

    Valid Anagram

---
+++ Arrays & Hashing/Valid Anagran.py
@@ -0,0 +1,28 @@
+def isAnagram(self, s: str, t: str) -> bool:
+    # String lengths are the same
+    if len(s) != len(t):
+        return False
+
+    freq_table1 = {}
+    freq_table2 = {}
+
+    for ind in range(len(s)):
+        char1 = s[ind]
+        char2 = t[ind]
+        in_table1 = freq_table1.get(char1)
+        in_table2 = freq_table2.get(char2)
+
+        if in_table1 is None:
+            freq_table1.update({char1: 1})
+        else:
+            freq_table1.update({char1: in_table1 + 1})
+
+        if in_table2 is None:
+            freq_table2.update({char2: 1})
+        else:
+            freq_table2.update({char2: in_table2 + 1})
+
+    if freq_table1 == freq_table2:
+        return True
+    else:
+        return False
