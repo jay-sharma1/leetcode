abc21f3f5d0d934e9048df10147e33b235455c3a
jay-sharma1 <64980964+jay-sharma1@users.noreply.github.com>
2025-03-10 17:18:31 -0400

    recursive solution maybe

--- Stack/Generate Parentheses.py
+++ Stack/Generate Parentheses.py
@@ -1,3 +1,12 @@
-class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
+def generateParenthesis(n: int) -> list[str]:
+    # Create a counter for however many parenthesis we have left
+    open = n - 1
+    close = n
+    result = []
+    
+    recursiveParenthesis
+    
+
+def recursiveParenthesis(open: list[str], close: list[str], input: str) -> list[str]:
+    pass
         
