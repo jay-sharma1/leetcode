38199d32a1fbf011a7fbfc7fcefcfd32c3a28016
jay-sharma1 <64980964+jay-sharma1@users.noreply.github.com>
2025-03-31 11:35:55 -0400

    valid palindrome done

--- Two Pointer/Valid Palindrome.py
+++ Two Pointer/Valid Palindrome.py
@@ -1,6 +1,37 @@
-def isPalindrome(s: str) -> bool:
-    pass
+import re
 
-# Takes a string input, removes all spaces, punctuaiton, and makes letters lowercase
-def processString(s: str) -> str:
-    pass
+# This is done to minimize space used in memory.
+def isPalindrome(s: str) -> bool:
+    leftPointer = 0
+    rightPointer = len(s) - 1
+    
+    leftString, rightString = None, None
+    
+    # Loop until pointers cross
+    while leftPointer <= rightPointer:
+        leftString = s[leftPointer]
+        rightString = s[rightPointer]
+        
+        leftIsAlpha = re.match(r"[A-Za-z0-9]", leftString)
+        rightIsAlpha = re.match(r"[A-Za-z0-9]", rightString)
+
+        # If left string is not an alphanumeric, move pointer to the right.
+        if not leftIsAlpha:
+            leftPointer += 1
+        
+        # If right string is not an alphanumeric, move pointer to the left.
+        if not rightIsAlpha:
+            rightPointer -= 1
+        
+        # If both are alphanumerics, compare the strings
+        if leftIsAlpha and rightIsAlpha:
+            if leftString.lower() != rightString.lower():
+                return False
+            else:
+                leftPointer += 1
+                rightPointer -= 1
+    
+    return True
+
+# Test
+print(isPalindrome(s = " "))
